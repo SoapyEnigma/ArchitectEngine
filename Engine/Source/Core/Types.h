88b8da1f8ebc09ab98b8de5b40024b7a5f072d21
Fraxinus94 <keithlekgolo@gmail.com>
2024-03-13 10:46:33 -0400

    First Day

---
+++ Engine/Source/Core/Types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using i8 = std::int8_t;
+using u8 = std::uint8_t;
+using i16 = std::int16_t;
+using u16 = std::uint16_t;
+using f16 = halffloat::half;
+using i32 = std::int32_t;
+using u32 = std::uint32_t;
+using f32 = float;
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+using f64 = double;
